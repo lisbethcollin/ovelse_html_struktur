7dc4e3e0f8cd7b837ea34ef892f2e98ee3e83b1e
Lisbeth Collin <lico0001@stud.kea.dk>
2023-08-24 14:59:14 +0200

    overlse 2b done

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const header = document.querySelector("header");
-console.log(header);
+const title = document.querySelector("h1");
+console.log(title);
+title.textContent = "Dette er overskriften";
 
 const billede = document.querySelector("img");
 console.log(billede);
@@ -12,3 +13,10 @@ console.log(art);
 
 const footer = document.querySelector("footer");
 console.log(footer);
+
+const underoverskriftH3 = document.querySelector("h3");
+//console.log(firstP.textContent);
+underoverskriftH3.innerHTML = "Katte næser";
+
+const secondP = document.querySelector("section article:nth-child(2) p");
+secondP.innerHTML = "<i>Hej med dig jeg hedder kaj</i>";
