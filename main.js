cfe9c61039f63b68f7b1f930093dea488d9c6ab8
Lisbeth Collin <lico0001@stud.kea.dk>
2023-08-24 10:33:42 +0200

    ovelse 2a

---
+++ main.js
@@ -0,0 +1,14 @@
+const header = document.querySelector("header");
+console.log(header);
+
+const billede = document.querySelector("img");
+console.log(billede);
+
+const tekst = document.querySelector("#tekst");
+console.log(tekst);
+
+const art = document.querySelector("art2");
+console.log(art);
+
+const footer = document.querySelector("footer");
+console.log(footer);
